616c3f26df6736af695c1445ae2ca471077ba263
ergz <erodriguez@hey.com>
2025-01-01 00:38:51 -0800

    wip

--- installers/ghostty.sh
+++ installers/ghostty.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-
 cd /tmp
 [ -d "ghostty" ] && rm -rf ghostty
-
-git clone https://github.com/ghostty-org/ghostty
+# Get the latest tag, cleaned up
+LATEST_TAG=$(git ls-remote --tags --sort="v:refname" https://github.com/ghostty-org/ghostty.git | tail -n1 | sed 's/.*\///' | sed 's/\^{}//')
+git clone --depth 1 --branch $LATEST_TAG https://github.com/ghostty-org/ghostty
 cd ghostty
 zig build -p $HOME/.local -Doptimize=ReleaseFast
 cd ..
