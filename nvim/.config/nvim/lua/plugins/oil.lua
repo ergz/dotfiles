536b6c77a4df8a9ad4c72821d458bd100432d902
ergz <erodriguez@flowwest.com>
2026-05-15 14:31:58 -0700

    sync

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,5 +1,6 @@
 require("oil").setup({
   default_file_explorer = true,
+  skip_confirm_for_simple_edits = true,
   keymaps = {
     ["<C-s>"] = false,
     ["<C-h>"] = { "actions.toggle_hidden", mode = "n" },
